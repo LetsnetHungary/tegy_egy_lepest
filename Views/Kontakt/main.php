d8886593e6bb5d8230f54180475e7e54083a5872
kovacsoliver <kovacs.oliver@letsnet.hu>
2017-09-24 17:53:21 +0200

    close to the end

--- Views/Kontakt/main.php
+++ Views/Kontakt/main.php
@@ -7,18 +7,18 @@
       <hr class="my-3">
     </div>
    </div>
-   <div class="container lead">
-      <div class="col-12 contact-info">
+   <div class="container">
+      <div class="col-12 contact-info lead">
          <i class="fa fa-location-arrow fa-1x" aria-hidden="true">  9999 budapest xy utca 99</i>
       </div>
-      <div class="col-12 contact-info">
+      <div class="col-12 contact-info lead">
          <i class="fa fa-user fa-1x" aria-hidden="true">  valaki valaki</i>
 
       </div>
-      <div class="col-12 contact-info">
+      <div class="col-12 contact-info lead">
    <i class="fa fa-phone fa-1x" aria-hidden="true">  12345678</i>
       </div>
-      <div class="col-12 contact-info">
+      <div class="col-12 contact-info lead">
    <i class="fa fa-envelope fa-1x" aria-hidden="true">  valaki@gmail.com</i>
       </div>
    </div>
--- Views/includes/main/header.php
+++ Views/includes/main/header.php
@@ -1,23 +1,4 @@
 <header style="opacity: 1; z-index: 2;">
-
-         <!--
-   <nav class="navbar fixed-top navbar-expand-sm navbar-light">
-      <div class="container">
-         <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
-            <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="collapse navbar-collapse" id="mainNav">
-            <div class="navbar-nav">
-               <a class="nav-item nav-link active" href="#">Home</a>
-               <a class="nav-item nav-link" href="#">About</a>
-               <a class="nav-item nav-link" href="#">Training</a>
-               <a class="nav-item nav-link" href="#">Contact</a>
-            </div>
-         </div>
-      </div>
-   </nav>
-
-   -->
 <div class="hero">
    <div class="container">
       <div class="row">
@@ -56,65 +37,7 @@
       </div>
    </div>
 </div>
-<div class="header-space" style="height: 74px;">
+<div class="header-space" style="height: 103px;">
 
 </div>
-
-   <!--
-   <nav class="navbar fixed-top navbar-expand-md navbar-light bg-faded">
-     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-       <span class="navbar-toggler-icon"></span>
-     </button>
-
-     //meg kéne szerezni, hogy aktív e a bemutatkozó oldal, és csak akkor használni a #about stb címeket, egyébként Index/#about
-
-     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-       <ul class="navbar-nav mr-auto">
-         //Image and text
-       <nav class="navbar navbar-light bg-faded">
-         <a class="navbar-brand" href="/">
-           <img src="/assets/images/Tegyegylepest_logo.jpg" style="margin-left: 0;"width="128" height="61" class="d-inline-block align-top" alt="">
-         </a>
-       </nav>
-         <li class="nav-item active dropdown">
-           <a class="navbar-brand dropdown-toggle" href="Index" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-             Bemutatkozás<span class="sr-only">(current)</span>
-           </a>
-           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-             <a class="dropdown-item" href="Index#about">Az alapítványról</a>
-             <a class="dropdown-item" href="Index#members">A kuratórium tagjai</a>
-             <a class="dropdown-item" href="Index#docs">Beszámolók</a>
-           </div>
-         </li>
-         <li class="nav-item">
-           <a class="navbar-brand" href="/Projektek">Támogatott projektek</a>
-         </li>
-         <li class="nav-item">
-           <a class="navbar-brand" href="/Kontakt">Elérhetőség</a>
-         </li>
-         <li class="nav-item">
-           <a class="navbar-brand" href="/ado">Adó 1% / támogatás</a>
-         </li>
-       </ul>
-     </div>
-   </nav>
-
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-      <nav class="navbar bg-danger navbar-toggleable-sm navbar-inverse">
-         <div class="container">
-            <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="mainNav">
-               <div class="navbar-nav">
-                  <a class="nav-item nav-link active" href="#">Home</a>
-                  <a class="nav-item nav-link" href="#">About</a>
-                  <a class="nav-item nav-link" href="#">Training</a>
-                  <a class="nav-item nav-link" href="#">Contact</a>
-               </div>
-            </div>
-         </div>
-      </nav>
-   -->
-
 </header>
