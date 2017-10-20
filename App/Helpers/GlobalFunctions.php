69b4e091b5a302a0aab3c2b92000dd8c5073a264
adambalassa <balassa.adam@letsnet.hu>
2017-10-21 01:25:29 +0200

    edr

--- App/Helpers/GlobalFunctions.php
+++ App/Helpers/GlobalFunctions.php
@@ -3,7 +3,7 @@ namespace Helpers;
 
 class GlobalFunctions {
   public static function getErrorMessage($code){
-    include_once("/App/Models/Login_Model.php");
+    include_once("App/Models/Login_Model.php");
     $model = new  \Login_Model();
     return $model->getErrorMessage($code);
   }
